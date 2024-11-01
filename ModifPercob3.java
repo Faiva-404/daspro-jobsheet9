fd34d8c8e6f8167819f9f6d30546676debc487ca
Faiva-404 <puspafai5@gmail.com>
2024-11-01 14:25:04 +0700

    Add files via upload

---
+++ ModifPercob3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class ModifPercob3 {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //deklarasi variabel total dan rata2
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+
+        //isi dgn nilai input dari pengguna
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+
+        //gunakan perulangan unt menghitung jml keseluruhan nilai
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus! ");
+            }
+        }
+
+        //hitung rata2
+        rata2 = total/nilaiMhs.length;
+        System.out.println("Rata-rata nilai = " + rata2);
+   } 
+}
